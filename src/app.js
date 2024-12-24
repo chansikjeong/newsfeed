efc2c6fec2dcf6dd8e032350b4f9e7bfa802311d
chansikjeong <cjyong1004@naver.com>
2024-12-24 11:52:29 +0900

    ✨ update  user라우터-회원가입 로그인 기능/ 검증 인증 인가 과정 추가

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import userRouter from './routers/user.router.js';
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,7 @@ const PORT = 3000;
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', [userRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
