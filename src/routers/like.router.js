2907df86996fc6661ff5f31b431ff686d088a14c
SmoothMonologue <mobugy99@gmail.com>
2024-12-26 16:14:57 +0900

    ♻️refactor: 함수명 변경, 응답 추가

--- src/routers/like.router.js
+++ src/routers/like.router.js
@@ -12,7 +12,7 @@ router.post('/post/:postId/like', authorization, async (req, res, next) => {
   const { postId } = req.params;
   try {
     //해당 유저가 이미 이 글에 좋아요를 눌렀는지 판단
-    const myLike = didYouLike(postId, userId);
+    const myLike = checkLike(postId, userId);
     //안 눌렀으면 좋아요 데이터 생성
     if (!myLike) {
       await prisma.like.create({
@@ -21,6 +21,9 @@ router.post('/post/:postId/like', authorization, async (req, res, next) => {
           userId,
         },
       });
+      return res
+          .status(201)
+          .json({ message: '좋아요!' });
     }
   } catch (err) {
     next(err);
@@ -33,7 +36,7 @@ router.delete('/post/:postId/like', authorization, async (req, res, next) => {
   const { postId } = req.params;
   try {
     //해당 유저가 이미 이 글에 좋아요를 눌렀는지 판단
-    const myLike = didYouLike(postId, userId);
+    const myLike = checkLike(postId, userId);
     //눌렀으면 좋아요 데이터 삭제
     if (myLike) {
       await prisma.like.delete({
@@ -42,6 +45,9 @@ router.delete('/post/:postId/like', authorization, async (req, res, next) => {
           userId,
         },
       });
+      return res
+          .status(200)
+          .json({ message: '사실 안 좋아요.' });
     }
   } catch (err) {
     next(err);
@@ -49,7 +55,7 @@ router.delete('/post/:postId/like', authorization, async (req, res, next) => {
 });
 
 /** 해당 좋아요를 찾는 함수, 오류 확인 필요 **/
-async function didYouLike(pId, uId) {
+async function checkLike(pId, uId) {
   return await prisma.like.findFirst({
     where: {
       postId: parseInt(pId),
