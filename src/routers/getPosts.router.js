2b5be5d976cb627e956cbe186aef8768d650d7dc
Parkcw00 <parkcw0325@gmail.com>
2024-12-26 16:07:48 +0900

    ✨ update: 모바일 게시글 전체 조회 기능

---
+++ src/routers/getPosts.router.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import { prisma } from '../utils/prisma/index.js';
+
+const router = express.Router();
+
+router.get('/posts/mobile', async (req, res, next) => {
+  try {
+    const mobilePosts = await prisma.posts.findMany({
+      where: { type: 'mobile' },
+      select: {
+        title: true,
+        content: true,
+        createdAt: true,
+        Users: {
+          select: {
+            nickname: true,
+          },
+        },
+        // 포스트 아이디와 같은 좋아요의 개수를 출력하고 싶다.
+        // 좋아요 테이블에 1번게시글이 들어간 개수가 10개라면
+        _count: {
+          select: {
+            Like: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    if (mobilePosts.length === 0) {
+      return res.status(404).json({ message: '게시글이 존재하지 않습니다.' });
+    }
+
+    const formattedPosts = mobilePosts.map((post) => ({
+      title: post.title,
+      content: post.content,
+      createdAt: post.createdAt,
+      nickname: post.Users.nickname,
+      likes: post._count.Like,
+    }));
+
+    return res
+      .status(200)
+      .json({ message: '게시글 조회가 완료되었습니다.', data: formattedPosts });
+  } catch (err) {
+    next(err);
+  }
+});
