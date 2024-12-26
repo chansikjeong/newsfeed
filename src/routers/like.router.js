32f677561d5e56cf505b39ba15aca1c1d63d9633
SmoothMonologue <mobugy99@gmail.com>
2024-12-26 20:08:49 +0900

    bugfix: 조건에 오류 있음

--- src/routers/like.router.js
+++ src/routers/like.router.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
 import authorization from '../middlewares/authorization.js';
 
 const router = express.Router();
@@ -9,23 +7,21 @@ const router = express.Router();
 /** 좋아요 꾸욱 **/
 router.post('/post/:postId/like', authorization, async (req, res, next) => {
   const userId = parseInt(req.user.id);
-  const { postId } = req.params;
+  const postId = parseInt(req.params.postId);
   try {
     //해당 유저가 이미 이 글에 좋아요를 눌렀는지 판단
-    const myLike = checkLike(postId, userId);
     //안 눌렀으면 좋아요 데이터 생성
-    if (!myLike) {
+    if (!checkLike(postId, userId)) {
       await prisma.like.create({
         data: {
           postId,
           userId,
         },
       });
-      return res
-          .status(201)
-          .json({ message: '좋아요!' });
+      return res.status(201).json({ message: '좋아요!' });
     }
   } catch (err) {
+    console.log(err);
     next(err);
   }
 });
@@ -36,18 +32,15 @@ router.delete('/post/:postId/like', authorization, async (req, res, next) => {
   const { postId } = req.params;
   try {
     //해당 유저가 이미 이 글에 좋아요를 눌렀는지 판단
-    const myLike = checkLike(postId, userId);
     //눌렀으면 좋아요 데이터 삭제
-    if (myLike) {
+    if (checkLike(postId, userId)) {
       await prisma.like.delete({
         data: {
           postId,
           userId,
         },
       });
-      return res
-          .status(200)
-          .json({ message: '사실 안 좋아요.' });
+      return res.status(200).json({ message: '사실 안 좋아요.' });
     }
   } catch (err) {
     next(err);
@@ -56,12 +49,13 @@ router.delete('/post/:postId/like', authorization, async (req, res, next) => {
 
 /** 해당 좋아요를 찾는 함수, 오류 확인 필요 **/
 async function checkLike(pId, uId) {
-  return await prisma.like.findFirst({
+  const myLike = await prisma.like.findFirst({
     where: {
       postId: parseInt(pId),
       userId: parseInt(uId),
     },
   });
+  return myLike == null ? false : true;
 }
 
 export default router;
